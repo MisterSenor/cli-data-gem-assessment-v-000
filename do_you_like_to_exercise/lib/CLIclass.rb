58ec665550f171807c30a9a71c5e8fa845724260
Nathan  Visak <nathan.visak@gmail.com>
2018-04-29 19:14:44 +0000

    Added more to the CLI skeleton

--- do_you_like_to_exercise/lib/CLIclass.rb
+++ do_you_like_to_exercise/lib/CLIclass.rb
@@ -9,7 +9,23 @@ class CLIclass
     input = nil
       while input != "exit"
         input = gets.strip
-        if input == "Y"
+        case input
+        when "Y"
           puts "How would you characterize your fitness level?  Type 'B' for beginner, 'I' for intermediate, and 'A' for Advanced."
-
+        when "B"
+          Crossfit_beginner_scraper #need to define this class
+        when "I"
+          Crossfit_intermediate_scraper #need to define this class
+        when "A"
+          Crossfit_advanced_scraper
+        when "N"
+          puts "Do you like ice cream? (Enter: 'Yes'/'No')"
+        when "Yes"
+          Culvers_flavor_scraper
+        when "No"
+          puts "I'm really sorry.  You don't like ice cream OR exercise.  Your life must be very difficult.  Type 'exit' to exit."
+        end
+      end
+    end
+          
 end
