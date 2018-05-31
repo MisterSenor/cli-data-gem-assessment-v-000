53ba6f9a43a86bef5838993d58b569fb6fc27035
Nathan  Visak <nathan.visak@gmail.com>
2018-05-31 15:07:42 +0000

    improved Cli object

--- do_you_like_to_exercise/lib/do_you_like_to_exercise/Cli.rb
+++ do_you_like_to_exercise/lib/do_you_like_to_exercise/Cli.rb
@@ -7,11 +7,14 @@ class Cli
      if input == "Y"
        puts "Hello and welcome to the Crossfit WOD scraper!"
        puts "Note: WOD stands for 'Workout of the Day.'"
-       puts "Type '1' for this weeks' series of WOD's."
-     elsif input == "1"
-       puts scraper object for WODs
+       puts "Type 'WOD' for this weeks' series of WOD's."
      elsif input == "N"
-       puts "Here is Culver's frozen custard flavor of the day: #{scrape the Culvers flavor for today}."
+       puts "Well, if you don't like to exercise maybe you would like to know that Culver's frozen custard flavor of the day is ____."
+     end
+     input = gets.strip
+     if input == "WOD"
+       puts "scraper object for WODs"
+       puts "Type the number of the WOD for a more specific description of the WOD."
      end
    end
 
