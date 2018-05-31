d947cba955a26f23c243eac887e42d8be18f378d
Nathan  Visak <nathan.visak@gmail.com>
2018-05-31 15:44:04 +0000

    don't know why these changes to my Cli object didn't commit last time, but here goes again

--- do_you_like_to_exercise/lib/Cli.rb
+++ do_you_like_to_exercise/lib/Cli.rb
@@ -1,33 +1,27 @@
 
-class Cli 
+class Cli
 
-  def initialize
-  end
 
   def call
     puts "Hello.  Do you like to exercise? (Enter: Y/N)"
-    input = nil
-      while input != "exit"
-        input = gets.strip
-        case input
-        when "Y"
-          puts "How would you characterize your fitness level?  Type 'B' for beginner, 'I' for intermediate, and 'A' for Advanced."
-        when "B"
-          puts "10 push ups"#Crossfit_beginner_scraper, use some hard-coded data before you move on to scraped data
-        when "I"
-          puts "10 pushups and 10 double unders, AMRAP" #Crossfit_intermediate_scraper, use some hard-coded data before you move on to scraped data
-        when "A"
-          puts "Murder your face off with an insane workout, AMRAP" #Crossfit_advanced scraper, use some hard-coded data before you move on to scraped data
-        when "N"
-          puts "Do you like ice cream? (Enter: 'Yes'/'No')"
-        when "Yes"
-          puts "Culvers flavor of the day is: 'Andes Mint Avalance'" #Culvers_flavor_scraper, use some hard-coded strings until you write your scraper
-        when "No"
-          puts "I'm really sorry.  You don't like ice cream OR exercise.  What do you like?"
-        when !"exit" && !"Y" && !"B" && !"I" && !"A" && !"N" && !"Yes" && !"No"
-          puts "Well, at least you like SOMETHING!  Type 'exit' to exit."
-        end
+    input = gets.strip
+    if input == "Y"
+      puts "Welcome to the Crossfit.com wod scraper!  W.o.d. stands for 'workout of the day'."
+      puts "Type wod for a list of this weeks wods."
+    elsif input == "N"
+      input = gets.strip
+      puts "Do you like ice cream? (Enter: nY/N)"
+      if input == "Y"
+        puts "Culver's frozen custard flavor of the day is _____."
+      elsif input == "N"
+        puts "So, you don't like to exercise, nor do you like ice cream.  Who are you?"
       end
     end
-
+    input = gets.strip
+    if input == "wod"
+      puts "Here is a list of the previous week's wods __________. Type the number of the wod for more specific information."
+    elsif input != "wod"
+      puts "Remember, to type wod for a list of this week's wods."
+    end
+    #will need to stub out the second level of interacting with the scraped data here
 end
