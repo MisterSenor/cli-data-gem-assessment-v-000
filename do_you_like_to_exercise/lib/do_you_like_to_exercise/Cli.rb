8bc41a82d2ed92503fec29908bee610dd67be29c
Nathan  Visak <nathan.visak@gmail.com>
2018-05-31 14:45:52 +0000

    fixed some problems with looping in the Cli object

--- do_you_like_to_exercise/lib/do_you_like_to_exercise/Cli.rb
+++ do_you_like_to_exercise/lib/do_you_like_to_exercise/Cli.rb
@@ -1,17 +1,18 @@
 
 class Cli
 
-
   def call
-    puts "Hello.  Do you like to exercise? (Enter: Y/N)"
-    input = gets.strip
-      while input != "exit"
-        puts "Hello and welcome to the Crossfit WOD scraper."
-        puts "Note: WOD stands for 'Workout of the Day'"
-        puts "Type 'week' for last weeks' series of WODs."
-        puts "Type 'month' for last months' series of WODs."
-      end
-    end
+     puts "Hello.  Do you like to exercise? (Enter: Y/N)"
+     input = gets.strip
+     if input == "Y"
+       puts "Hello and welcome to the Crossfit WOD scraper!"
+       puts "Note: WOD stands for 'Workout of the Day.'"
+       puts "Type '1' for this weeks' series of WOD's."
+     elsif input == "1"
+       puts scraper object for WODs
+     elsif input == "N"
+       puts "Here is Culver's frozen custard flavor of the day: #{scrape the Culvers flavor for today}."
+     end
+   end
 
-
-end
+ end
