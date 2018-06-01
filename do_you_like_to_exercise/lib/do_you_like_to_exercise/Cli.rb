9eb2693bb64cf0708df4c0cfbe64a907c72483dd
Nathan  Visak <nathan.visak@gmail.com>
2018-06-01 16:30:26 +0000

    small change to Cli class

--- do_you_like_to_exercise/lib/do_you_like_to_exercise/Cli.rb
+++ do_you_like_to_exercise/lib/do_you_like_to_exercise/Cli.rb
@@ -10,16 +10,25 @@ class Cli
        puts "Type 'WOD' for this weeks' series of WOD's."
      elsif input == "N"
        puts "Well, if you don't like to exercise maybe you would like to know that Culver's frozen custard flavor of the day is ____."
+     elsif input == "exit"
+       goodbye
      end
      input = gets.strip
      if input == "WOD"
-       week_of_wods
+       list_wods
        puts "Type the number of the WOD for a more specific description of the WOD."
+     elsif input == "exit"
+       goodbye
      end
    end
 
-   def week_of_wods
+   def list_wods
      #this method will scrape up a weeks' worth of wods
+     puts "wod 1, wod 2, wod 3, wod 4, wod 5, wod 6, wod 7"
+   end
+
+   def goodbye
+     puts "See you next time for more wods!"
    end
 
 
