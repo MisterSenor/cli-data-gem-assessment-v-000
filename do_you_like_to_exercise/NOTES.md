ffa2b0475d52d18b93895416d7a0cd607c73da4e
Nathan  Visak <nathan.visak@gmail.com>
2018-06-01 21:00:32 +0000

    starting to flesh out Wod class

---
+++ do_you_like_to_exercise/NOTES.md
@@ -0,0 +1,2 @@
+What is a wod?
+  A wod has a date, a wod has a day_of_the_week, a wod has a name, and wod has a description 
---
+++ do_you_like_to_exercise/lib/do_you_like_to_exercise/Wod.rb
@@ -0,0 +1,11 @@
+
+class Wod
+  attr_accessor :date, :day, :name, :description
+
+  wod_1 = self.new
+  wod_1.date =
+  wod_1.day =
+  wod_1.name =
+  wod_1.description = 
+
+end
