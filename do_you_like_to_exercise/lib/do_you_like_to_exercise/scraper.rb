5bc9ea662d430ccb6f8bc93f16f959a04b0035bf
Nathan  Visak <nathan.visak@gmail.com>
2018-06-05 20:04:27 +0000

    starting to make the Scraper class

--- do_you_like_to_exercise/lib/do_you_like_to_exercise/scraper.rb
+++ do_you_like_to_exercise/lib/do_you_like_to_exercise/scraper.rb
@@ -2,17 +2,19 @@
 
 class Scraper
 
-  def get_page
-    Nokogiri::HTML(open("https://www.crossfit.com/workout"))
+  def self.get_page
+    doc = Nokogiri::HTML(open("https://www.crossfit.com/workout"))
+    day_with_date = doc.search("h3.hide").text
   end
 
+  day = doc.search("h3.hide").text
 
-  def scrape_wods_index
-    self.get_page.css("div#col-xs-12 col-sm-6 col-md-7 col-lg-7 content")
-  end
-
-  def make_wods
-  end
+  # def scrape_wods_index
+  #   self.get_page.css("div#col-xs-12 col-sm-6 col-md-7 col-lg-7 content")
+  # end
+  #
+  # def make_wods
+  # end
 
 end
 
