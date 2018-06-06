6c2c88a633ffaa33c69cfa29c177c2701df89b31
Nathan  Visak <nathan.visak@gmail.com>
2018-06-06 20:44:33 +0000

    still working on my Scraper class

--- do_you_like_to_exercise/lib/do_you_like_to_exercise/scraper.rb
+++ do_you_like_to_exercise/lib/do_you_like_to_exercise/scraper.rb
@@ -2,18 +2,27 @@
 
 class Scraper
 
-  def self.get_page
+  def self.get_days_and_dates
     doc = Nokogiri::HTML(open("https://www.crossfit.com/workout"))
-    day_with_date = doc.search("h3.hide").text
-    name = doc.search("div.col-sm-6 p strong").text #this is returning waaaay more than what I want.  I'm going to have to save these nodes into an array and pull out what I need, I think.
+    day_with_date = doc.search("h3.show").text
+    day_and_date_array = day_with_date.split(" ")
+    day_and_date_array.collect do |i|
+      i
+    end
   end
 
-  def day_with_date
-    self.get_page
-    day_of_the_week_and_date = doc.search("h3.hide").text
-    day_of_the_week_and_date[0..6]
-    binding.pry
+
+
+  def self.get_wods
+    doc = Nokogiri::HTML(open("https://www.crossfit.com/workout"))
+    wods = doc.search("div.col-sm-6 p strong").text
   end
+
+
+  #I KEEP GETTING THIS ERROR WHEN I TRY TO RUN THE DAY_WITH_DATE METHOD IN THE CONSOLE: Scraper.day_with_date
+# NoMethodError: undefined method `day_with_date' for Scraper:Class
+#         from (irb):2
+#         from ./console:13:in `<main>'
 
 
   # def scrape_wods_index
