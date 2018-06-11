16ae14f8abbdddbbcb9e00f3da043423f47085ab
Nathan  Visak <nathan.visak@gmail.com>
2018-06-11 15:42:57 +0000

    some changes to wod scraper

--- do_you_like_to_exercise/lib/do_you_like_to_exercise/scraper.rb
+++ do_you_like_to_exercise/lib/do_you_like_to_exercise/scraper.rb
@@ -17,25 +17,17 @@ class Scraper
   def self.get_wods
     wod_array = []
     doc = Nokogiri::HTML(open("https://www.crossfit.com/workout"))
-    wods = doc.search("div.col-sm-6")
-    wods.map do |each_wod|
-      wod_array << each_wod.text
-    end
-    wod_array[0..6]
+    wods = doc.search("div.row div.col-sm-6")
+    days = doc.search("div.col-xs-12 col-sm-6 col-m-7 col-lg-7 content")
+    wod_count = wods.count
+    day_count = days.count
+    binding.pry
+
   end
 
-
-  #I KEEP GETTING THIS ERROR WHEN I TRY TO RUN THE DAY_WITH_DATE METHOD IN THE CONSOLE: Scraper.day_with_date
-# NoMethodError: undefined method `day_with_date' for Scraper:Class
-#         from (irb):2
-#         from ./console:13:in `<main>'
+  The number of div.col-sm-6 needs to be divided by the number of div.row that they have as a parent
 
 
-  # def scrape_wods_index
-  #   self.get_page.css("div#col-xs-12 col-sm-6 col-md-7 col-lg-7 content")
-  # end
-  #
-  # def make_wods
-  # end
+
 
 end
