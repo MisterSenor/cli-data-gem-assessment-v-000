e28c8a0b15a3309fdb9e0026e7746a640d24ec6c
Nathan  Visak <nathan.visak@gmail.com>
2018-06-14 17:49:20 +0000

    small changes to Wod and Scraper classes

--- do_you_like_to_exercise/lib/do_you_like_to_exercise/Wod.rb
+++ do_you_like_to_exercise/lib/do_you_like_to_exercise/Wod.rb
@@ -1,9 +1,9 @@
 
-class Wod
+class DoYouLikeToExercise::Wod
   attr_accessor :day_and_date, :description
   @@all = []
 
-  def initialize(day_and_date=nil, description = nil)
+  def initialize(day_and_date=nil, description=nil)
     @day_and_date = day_and_date
     @description = description
     @@all << self
@@ -14,21 +14,21 @@ class Wod
   end
 
 
-  wod_1 = self.new
-  wod_1.date = "06/01/18" #formatted 180601
-  wod_1.day = "Friday"
-  wod_1.description = "3 rounds for time of: 9 muscle-ups, 36-ft. handstand walk, 36-45-54 single-leg squats, 36-ft. handstand walk"
-
-  wod_2 = self.new
-  wod_2.date = "05/31/18" #formatted 180531
-  wod_2.day = "Thursday"
-  wod_2.description = "Front squat 3-3-3 reps, Push press 3-3-3 reps, Thruster 3-3-3 reps"
-
-  wod_3 = self.new
-  wod_3.date = "?"
-  wod_3.day = "?"
-  wod_3.description = "?"
-
-  [wod_1, wod_2, wod_3]
+  # wod_1 = self.new
+  # wod_1.date = "06/01/18" #formatted 180601
+  # wod_1.day = "Friday"
+  # wod_1.description = "3 rounds for time of: 9 muscle-ups, 36-ft. handstand walk, 36-45-54 single-leg squats, 36-ft. handstand walk"
+  #
+  # wod_2 = self.new
+  # wod_2.date = "05/31/18" #formatted 180531
+  # wod_2.day = "Thursday"
+  # wod_2.description = "Front squat 3-3-3 reps, Push press 3-3-3 reps, Thruster 3-3-3 reps"
+  #
+  # wod_3 = self.new
+  # wod_3.date = "?"
+  # wod_3.day = "?"
+  # wod_3.description = "?"
+  #
+  # [wod_1, wod_2, wod_3]
 
 end
--- do_you_like_to_exercise/lib/do_you_like_to_exercise/scraper.rb
+++ do_you_like_to_exercise/lib/do_you_like_to_exercise/scraper.rb
@@ -1,16 +1,15 @@
 
-
 class Scraper
 
-  def self.get_days_and_dates
-    day_date_array = []
-    doc = Nokogiri::HTML(open("https://www.crossfit.com/workout"))
-    day_with_date = doc.search("h3.hide a")
-    day_with_date.map do |day_date|
-      day_date_array << day_date.text
-    end
-    day_date_array[0..6]
-  end
+  # def self.get_days_and_dates
+  #   day_date_array = []
+  #   doc = Nokogiri::HTML(open("https://www.crossfit.com/workout"))
+  #   day_with_date = doc.search("h3.hide a")
+  #   day_with_date.map do |day_date|
+  #     day_date_array << day_date.text
+  #   end
+  #   day_date_array[0..6]
+  # end
 
 
 
@@ -20,12 +19,7 @@ class Scraper
     wods.each do |wod|
       day_and_date = wod.search("h3.hide a").text
       description = wod.search("div.col-sm-6").text
-      Wod.new(day_and_date, description).save
-
-      # wod.search(?) = day
-      # wod.search() = description
-      # wod.search(?) = title
-      binding.pry
+      Wod.new(day_and_date, description)
       # Wod.new_from_index_page(wod)
     end
   end
