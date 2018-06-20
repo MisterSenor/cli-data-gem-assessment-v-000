89a3c6301fa66f01a54baf61f4c4f4abca1edd57
Nathan  Visak <nathan.visak@gmail.com>
2018-06-20 19:55:23 +0000

    going in a different direction and correcting some anti-patersn

--- do_you_like_to_exercise/lib/do_you_like_to_exercise/Wod.rb
+++ do_you_like_to_exercise/lib/do_you_like_to_exercise/Wod.rb
@@ -15,17 +15,17 @@ class Wod
     @@all
   end
 
-  def self.scrape_descriptions
-    doc = Nokogiri::HTML(open("https://www.crossfit.com/workout"))
-    doc.search("div.col-xs-12.col-sm-6.col-md-7.col-lg-7.content").each do |row|
-      row.search("div.row").each do |workout_details|
-         new_wod = self.new
-         new_wod.description = workout_details.text
-         @@all << new_wod
-       end
-     end
-     @@all
-  end
+  # def self.scrape_descriptions
+  #   doc = Nokogiri::HTML(open("https://www.crossfit.com/workout"))
+  #   doc.search("div.col-xs-12.col-sm-6.col-md-7.col-lg-7.content").each do |row|
+  #     row.search("div.row").each do |workout_details|
+  #        new_wod = self.new
+  #        new_wod.description = workout_details.text
+  #        @@all << new_wod
+  #      end
+  #    end
+  #    @@all
+  # end
 
   def self.all
     @@all
--- do_you_like_to_exercise/lib/do_you_like_to_exercise/scraper.rb
+++ do_you_like_to_exercise/lib/do_you_like_to_exercise/scraper.rb
@@ -1,40 +1,15 @@
 
 class Scraper
 
-  # def self.get_days_and_dates
-  #   day_date_array = []
-  #   doc = Nokogiri::HTML(open("https://www.crossfit.com/workout"))
-  #   day_with_date = doc.search("h3.hide a")
-  #   day_with_date.map do |day_date|
-  #     day_date_array << day_date.text
-  #   end
-  #   day_date_array[0..6]
-  # end
-
-
-  def scrape_wods
+  def self.scrape_wods
     @doc = Nokogiri::HTML(open("https://www.crossfit.com/workout"))
     @doc.search("section#archives.section").each do |wod|
+      url = wod.search("h3.hide a").attr("href")
       day_and_date = wod.search("h3.hide a").text
       description = wod.search("div.col-xs-12.col-sm-6.col-md-7.col-lg-7.content div.row").text
+      binding.pry
     end
-  end 
-
-
-  def self.get_wods
-    new_array = []
-    doc = Nokogiri::HTML(open("https://www.crossfit.com/workout"))
-    wods = doc.search("section#archives.section")
-    wods.each do |wod|
-      day_and_date = wod.search("h3.hide a").text
-      description = wod.search("div.col-sm-6").text
-      new_array << wod
-      # Wod.new_from_index_page(wod)
-    end
-    new_array
   end
-
-
 
 
 end
