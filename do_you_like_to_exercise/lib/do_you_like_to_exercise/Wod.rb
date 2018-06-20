ea7510ed0b957cbc6d0924bb2760f399e885932b
Nathan  Visak <nathan.visak@gmail.com>
2018-06-20 19:21:31 +0000

    added some new initialization function within the Wod class

--- do_you_like_to_exercise/lib/do_you_like_to_exercise/Wod.rb
+++ do_you_like_to_exercise/lib/do_you_like_to_exercise/Wod.rb
@@ -3,39 +3,29 @@ class Wod
   attr_accessor :day_and_date, :description
   @@all = []
 
-
-  def initialize(day_and_date=nil, description=nil)
-    @day_and_date = day_and_date
-    @description = description
-  end
-
-  def self.scrape_days_and_dates
-    day_and_date_array = []
+  def self.scrape_and_initialize_wods
     doc = Nokogiri::HTML(open("https://www.crossfit.com/workout"))
     doc.search("div.container-hybrid").each do |row|
       row.search("h3.hide a").each do |day|
-        day_and_date_array << day.text
+        new_wod = self.new
+        new_wod.day_and_date = day.text
+        @@all << new_wod
       end
     end
-    day_and_date_array
+    @@all
   end
 
   def self.scrape_descriptions
     doc = Nokogiri::HTML(open("https://www.crossfit.com/workout"))
     doc.search("div.col-xs-12.col-sm-6.col-md-7.col-lg-7.content").each do |row|
-      row.search("div.row").each do |description|
-         new_wod = self.new(day_and_date=nil, description)
+      row.search("div.row").each do |workout_details|
+         new_wod = self.new
+         new_wod.description = workout_details.text
          @@all << new_wod
        end
      end
      @@all
   end
-
-
-
-
-
-
 
   def self.all
     @@all
