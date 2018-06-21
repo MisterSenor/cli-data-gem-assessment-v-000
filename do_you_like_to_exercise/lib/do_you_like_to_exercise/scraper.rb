aaf00680aae5de9f4129bd713e03a8d92a1030cc
Nathan  Visak <nathan.visak@gmail.com>
2018-06-21 20:33:49 +0000

    refactoring the scraper method to be smoother

--- do_you_like_to_exercise/lib/do_you_like_to_exercise/scraper.rb
+++ do_you_like_to_exercise/lib/do_you_like_to_exercise/scraper.rb
@@ -1,13 +1,14 @@
 
 class Scraper
 
-  def self.scrape_wods
+  def scrape_wods
     @doc = Nokogiri::HTML(open("https://www.crossfit.com/workout"))
-    @doc.search("section#archives.section").each do |wod|
-      url = wod.search("h3.hide a").attr("href")
-      day_and_date = wod.search("h3.hide a").text
-      description = wod.search("div.col-xs-12.col-sm-6.col-md-7.col-lg-7.content div.row").text
-      binding.pry
+    @doc.search("section#archives.section").each do |workout|
+    wod = Wod.new 
+    wod.url = workout.search("h3.hide a").attr("href")
+    wod.day_and_date = workout.search("h3.hide a").text
+    wod.description = workout.search("div.col-xs-12.col-sm-6.col-md-7.col-lg-7.content div.row").text
+    wod.save
     end
   end
 
