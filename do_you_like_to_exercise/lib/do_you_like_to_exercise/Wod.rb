8802ffde374d82a9b5c6d26d54b6c663f1b5df79
Nathan  Visak <nathan.visak@gmail.com>
2018-06-22 16:32:35 +0000

    refactored the Scraper class

--- do_you_like_to_exercise/lib/do_you_like_to_exercise/Wod.rb
+++ do_you_like_to_exercise/lib/do_you_like_to_exercise/Wod.rb
@@ -3,29 +3,9 @@ class Wod
   attr_accessor :day_and_date, :description
   @@all = []
 
-  def self.scrape_and_initialize_wods
-    doc = Nokogiri::HTML(open("https://www.crossfit.com/workout"))
-    doc.search("div.container-hybrid").each do |row|
-      row.search("h3.hide a").each do |day|
-        new_wod = self.new
-        new_wod.day_and_date = day.text
-        @@all << new_wod
-      end
-    end
-    @@all
+  def save
+    @@all << self
   end
-
-  # def self.scrape_descriptions
-  #   doc = Nokogiri::HTML(open("https://www.crossfit.com/workout"))
-  #   doc.search("div.col-xs-12.col-sm-6.col-md-7.col-lg-7.content").each do |row|
-  #     row.search("div.row").each do |workout_details|
-  #        new_wod = self.new
-  #        new_wod.description = workout_details.text
-  #        @@all << new_wod
-  #      end
-  #    end
-  #    @@all
-  # end
 
   def self.all
     @@all
--- do_you_like_to_exercise/lib/do_you_like_to_exercise/scraper.rb
+++ do_you_like_to_exercise/lib/do_you_like_to_exercise/scraper.rb
@@ -3,14 +3,15 @@ class Scraper
 
   def scrape_wods
     @doc = Nokogiri::HTML(open("https://www.crossfit.com/workout"))
-    @doc.search("section#archives.section").each do |workout|
-    wod = Wod.new 
-    wod.url = workout.search("h3.hide a").attr("href")
+    @doc.search("div.container-hybrid div.row").each do |workout|
+    wod = Wod.new
+    #wod.url = workout.search("h3.hide a").attr("href")
     wod.day_and_date = workout.search("h3.hide a").text
-    wod.description = workout.search("div.col-xs-12.col-sm-6.col-md-7.col-lg-7.content div.row").text
+    wod.description = workout.search("div.col-xs-12.col-sm-6.col-md-7.col-lg-7.content div.row").text.gsub("\n", "")
     wod.save
     end
+    Wod.all
   end
 
-
+#This method of iteration doesn't save distinct workouts by day, it stores all of the info in one index of the @all array. How do I fix this?
 end
