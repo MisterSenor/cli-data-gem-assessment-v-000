d40e089b7b4cff2782d418147294f261c1824041
Nathan  Visak <nathan.visak@gmail.com>
2018-06-25 20:30:11 +0000

    fixing a problem in the CLI

--- do_you_like_to_exercise/lib/do_you_like_to_exercise/Cli.rb
+++ do_you_like_to_exercise/lib/do_you_like_to_exercise/Cli.rb
@@ -10,7 +10,7 @@ class Cli
        puts "Note: WOD stands for 'Workout of the Day.'"
        puts "Here is a list of the previous weeks' wods."
        list_wods
-       menu
+       describe_wods
      elsif input == "N"
        puts "Well, if you don't like to exercise maybe you would like to know that Culver's frozen custard flavor of the day is ____."
      elsif input == "exit"
@@ -18,27 +18,28 @@ class Cli
      end
    end
 
-   def menu
-     puts "Enter the number of the wod for a more detailed description."
-     input = gets.strip
-      if input.to_i > 0 && input.to_i < 8
-        puts Wod.all[input.to_i - 1]
-        menu
-      elsif input == "exit"
-        goodbye
-      else
-        puts "Type a number from 1 to 7 or the word exit."
-        menu
-      end
-    end
-
-
-
 def list_wods
   #this method will scrape up a weeks' worth of wods
   Wod.all[0..6].each do |wod|
     puts wod.day_and_date
   end
+end
+
+def describe_wods
+  puts "Enter a number from 1 to 7 for more information on a workout."
+  week_of_wods_array = []
+  Wod.all[0..6].each do |wod|
+    week_of_wods_array << wod.description
+  end
+  input = gets.strip.to_i
+    if input > 0 && input < 8
+      puts week_of_wods_array[input.to_i + 1]
+    elsif input == "exit"
+      goodbye
+    else
+      puts "You need to type a number from 1 to 7."
+      describe_wods
+    end
 end
 
 def goodbye
