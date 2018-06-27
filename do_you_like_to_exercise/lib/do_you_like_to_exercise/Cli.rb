ebc9a2c22f70170c80a8f050cf8f397d04e51504
Nathan  Visak <nathan.visak@gmail.com>
2018-06-27 20:38:25 +0000

    fixed Cli controller data

--- do_you_like_to_exercise/lib/do_you_like_to_exercise/Cli.rb
+++ do_you_like_to_exercise/lib/do_you_like_to_exercise/Cli.rb
@@ -7,45 +7,55 @@ class Cli
      input = gets.strip
      if input == "Y"
        puts "Hello and welcome to the Crossfit WOD scraper!"
+       puts " "
        puts "Note: WOD stands for 'Workout of the Day.'"
+       puts " "
        puts "Here is a list of the previous weeks' wods."
+       puts " "
        list_wods
-       describe_wods
+       puts " "
+       puts "Type a number from 1 to 7 for more information on any of these wods."
      elsif input == "N"
        puts "Well, if you don't like to exercise maybe you would like to know that Culver's frozen custard flavor of the day is ____."
      elsif input == "exit"
        goodbye
      end
+      get_wods(input)
    end
 
-def list_wods
-  #this method will scrape up a weeks' worth of wods
-  Wod.all[0..6].each_with_index do |wod|
-    puts wod.day_and_date
-  end
-end
+   def list_wods
+     #this method will scrape up a weeks' worth of wods
+     Wod.all[0..6].each_with_index do |wod|
+       puts wod.day_and_date
+     end
+   end
 
-def describe_wods
-  puts "Enter a number from 1 to 7 for more information on a workout."
-  week_of_wods_array = []
-  Wod.all[0..6].each do |wod|
-    week_of_wods_array << wod.description
-  end
+  def get_wods(input)
+    week_of_wods_array = []
+    Wod.all[0..6].each do |wod|
+      week_of_wods_array << wod.description
+    end
     input = gets.strip
-    binding.pry
-    if input.to_i < 0 && input.to_i > 8
-
+    if input.to_i > 0 && input.to_i < 8
       puts week_of_wods_array[input.to_i - 1]
+      puts " "
     elsif input == "exit"
       goodbye
-    else
-      "Enter a number from 1 to 7 for more information on a workout."
     end
-end
+    puts "Would you like more information on these wods? (Y/N)"
+    puts " "
+    list_wods
+    input = gets.strip.upcase
+    if input == "Y"
+      get_wods(input)
+    else
+      goodbye
+    end
+  end
 
 
-def goodbye
-  puts "See you next time for more wods!"
-end
+  def goodbye
+    puts "See you tomorrow for more wods!"
+  end
 
 end
